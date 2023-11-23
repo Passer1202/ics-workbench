ba1750a94eb7139159b5ce0f235c58ba6b02e5df
tracer-nju <tracer@nju.edu.cn>
2023-11-23 21:10:02 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: 5d9352b2516e44e3b36c42531140c6cc   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:10:02 up  2:51,  1 user,  load average: 1.21, 0.80, 0.47

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -15,8 +15,9 @@ inline int64_t asm_add(int64_t a, int64_t b)
 }
 
 inline int asm_popcnt(uint64_t x){
-    int ans;//0
+    int ans=0;//0
     //x       1
+    
     asm volatile(
 
         "movl $0,%%eax;"
@@ -33,21 +34,22 @@ inline int asm_popcnt(uint64_t x){
         :"=a"(ans) 
         :"c"(x)
     );
+    
     return ans;
 }
 
-void *asm_memcpy(void *dest, const void *src, size_t n) {
+inline void *asm_memcpy(void *dest, const void *src, size_t n) {
   //return memcpy(dest, src, n);
-  //return dest;
+  return dest;
 }
 
 
-int asm_setjmp(asm_jmp_buf env) {
+inline int asm_setjmp(asm_jmp_buf env) {
   //return setjmp(env);
   return 0;
 }
 
-void asm_longjmp(asm_jmp_buf env, int val) {
+inline void asm_longjmp(asm_jmp_buf env, int val) {
   //longjmp(env, val);
   
 }
--- asm/main.c
+++ asm/main.c
@@ -5,6 +5,8 @@
 int main() {
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
+  assert(asm_popcnt(0x0123456789abcdefULL)==32);
+  printf("%d",asm_popcnt(0x0123456789abcdefULL));
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     //printf("%ld",asm_add(1234, 5678));
