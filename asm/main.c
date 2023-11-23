1baaabb11368b51b119d82bf018aa6d14fab0b78
tracer-nju <tracer@nju.edu.cn>
2023-11-23 20:59:36 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: 5d9352b2516e44e3b36c42531140c6cc   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  20:59:36 up  2:41,  1 user,  load average: 0.24, 0.29, 0.22

--- asm/main.c
+++ asm/main.c
@@ -5,12 +5,12 @@
 int main() {
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
+  printf("%d",asm_popcnt(0x0123456789abcdefULL));
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     //printf("%ld",asm_add(1234, 5678));
     // TODO: add more tests here.
     assert(asm_popcnt(0x0123456789abcdefULL)==32);
-    printf("%d",asm_popcnt(0x0123456789abcdefULL));
     asm_longjmp(buf, 123);
   } else {
    assert(r == 123);
