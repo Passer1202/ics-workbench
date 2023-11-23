b12558063fd02f907a3f81ff2b35c8af2f67603d
tracer-nju <tracer@nju.edu.cn>
2023-11-23 16:28:05 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: c1fc3112a56246eaa3bc8b04d3954f0c   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:28:05 up  2:30,  1 user,  load average: 0.66, 0.81, 0.70

--- asm/asm.h
+++ asm/asm.h
@@ -6,7 +6,16 @@
 #define asm_jmp_buf jmp_buf
 
 
-int64_t asm_add(int64_t a, int64_t b);
+int64_t asm_add(int64_t a, int64_t b){
+    int64_t c;
+    asm volatile(
+        "addq %1,%2;"
+        "movq %2,%0;"
+        :"=r"(c)
+        :"r"(a),"r"(b)
+    )
+    return c;
+};
 // 返回无符号 64 位整数 x 二进制表示中 1 的数量
 //int asm_popcnt(uint64_t x);
 // C 标准库中的 memcpy，用于复制两段不相交的内存
