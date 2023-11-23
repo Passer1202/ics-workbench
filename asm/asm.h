21771c787e114575c5b82bc63fb34b2e6caf779b
tracer-nju <tracer@nju.edu.cn>
2023-11-23 15:39:37 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: c1fc3112a56246eaa3bc8b04d3954f0c   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:39:37 up  1:41,  1 user,  load average: 0.81, 0.76, 0.78

--- asm/asm.h
+++ asm/asm.h
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 // TODO: replace with your definition
-//#include <setjmp.h> // remove this
+#include <setjmp.h> // remove this
 #define asm_jmp_buf jmp_buf
 
 int64_t asm_add(int64_t a, int64_t b){
@@ -14,19 +14,15 @@ int64_t asm_add(int64_t a, int64_t b){
     );
   return a+b;
 };
+
 int     asm_popcnt(uint64_t x){
-    int s = 0;
-    for (int i = 0; i < 64; i++) {
-        if ((x >> i) & 1) s++;
-    }
-    return s;
 };
 void   *asm_memcpy(void *dest, const void *src, size_t n){
-    return memcpy(dest, src, n);
+   
 };
 int     asm_setjmp(asm_jmp_buf env){
-    return setjmp(env);
+   
 };
 void    asm_longjmp(asm_jmp_buf env, int val){
-    longjmp(env, val);
+   
 };
