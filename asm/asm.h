48652c2cfe66be6449e26c77becc0bd9911185ac
tracer-nju <tracer@nju.edu.cn>
2023-11-23 15:47:29 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: c1fc3112a56246eaa3bc8b04d3954f0c   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:47:29 up  1:49,  1 user,  load average: 0.44, 0.49, 0.63

--- asm/asm.h
+++ asm/asm.h
@@ -2,9 +2,9 @@
 #include <stddef.h>
 
 // TODO: replace with your definition
-#include <setjmp.h> // remove this
+//#include <setjmp.h> // remove this
 #define asm_jmp_buf jmp_buf
-
+/*
 int64_t volatile asm_add(int64_t a, int64_t b){
     int c;
     asm volatile(
@@ -14,8 +14,29 @@ int64_t volatile asm_add(int64_t a, int64_t b){
     );
   return a+b;
 };
+*/
+int64_t asm_add(int64_t a, int64_t b) 
+{
+  return a+b;
+};
+// 返回无符号 64 位整数 x 二进制表示中 1 的数量
+int asm_popcnt(uint64_t x) {
+  int s = 0;
+  for (int i = 0; i < 64; i++) {
+    if ((x >> i) & 1) s++;
+  }
+  return s;
+}
+// C 标准库中的 memcpy，用于复制两段不相交的内存
+void *asm_memcpy(void *dest, const void *src, size_t n) {
+  return memcpy(dest, src, n);
+}
 
-int     asm_popcnt(uint64_t x);
-void   *asm_memcpy(void *dest, const void *src, size_t n);
-int     asm_setjmp(asm_jmp_buf env);
-void    asm_longjmp(asm_jmp_buf env, int val);
+// C 标准库中的 setjmp/longjmp，用于控制流长跳转
+int asm_setjmp(asm_jmp_buf env) {
+  return setjmp(env);
+}
+
+void asm_longjmp(asm_jmp_buf env, int val) {
+  longjmp(env, val);
+}
