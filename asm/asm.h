ca24645efc3ede3d2348e780068e4cd7cbee04f3
tracer-nju <tracer@nju.edu.cn>
2023-11-25 17:09:28 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: 4795b2d29ffc4dc4a5d2823bcdafd9d2   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:09:28 up 16 min,  1 user,  load average: 0.12, 0.29, 0.29

--- asm/asm.h
+++ asm/asm.h
@@ -3,9 +3,23 @@
 
 // TODO: replace with your definition
 //#include <setjmp.h> // remove this
+/*
+typedef  struct
+{
+ unsigned long long __rbx; // 通用数据寄存器之一
+ unsigned long long __rsp; // 栈指针寄存器
+ unsigned long long __rbp; // 基址指针寄存器
+ unsigned long long __r12;
+ unsigned long long __r13;
+ unsigned long long __r14;
+ unsigned long long __r15;
+ unsigned long long __rip;
+}jmp_buf[1];
+*/
 
-typedef unsigned long long jmp_buf[8];
-#define asm_jmp_buf jmp_buf
+typedef unsigned long long myjmp_buf[8];
+
+#define asm_jmp_buf myjmp_buf
 
 //struct int[16];
 int64_t asm_add(int64_t a, int64_t b);
