1e4c24c9563af18821ea2ed93e9576027bd96f28
tracer-nju <tracer@nju.edu.cn>
2023-12-09 11:39:53 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: c422606e9f174be4a5a61af46af8b7d8   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  11:39:53 up 3 min,  1 user,  load average: 0.49, 0.46, 0.20

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -8,7 +8,7 @@
 static bool is_prime[N];
 static int  primes[N]; 
 
-inline int *sieve(int n) {
+int *sieve(int n) {
   assert(n + 1 < N);
   //for (int i = 0; i <= n; i++)
     //is_prime[i] = true;
