f931387ef577a87fac4b0a9d7cdce40c047dee33
tracer-nju <tracer@nju.edu.cn>
2023-12-09 11:18:56 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: c81c31b2e77449368494fb9191c3322c   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  11:18:56 up 7 min,  1 user,  load average: 0.15, 0.32, 0.24

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -14,7 +14,7 @@ int *sieve(int n) {
     is_prime[i] = true;
 
   for (int i = 2; i <= n; i++) {
-    if(!is_prime[i])continue;
+    //if(!is_prime[i])continue;
     for (int j = i + i; j <= n; j += i) {
       is_prime[j] = false;
     }
