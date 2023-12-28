6ebe5333f0b2043d70358a427899a8ab6a6b9a45
tracer-nju <tracer@nju.edu.cn>
2023-12-28 21:04:39 +0800

    Static hostname: hengji-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 0a9bfe8e29a54a71baa93efb00649994          Boot ID: f4a693a074944e269aeda7c48c275b4f   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-39-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:04:39 up 43 min,  1 user,  load average: 1.06, 1.33, 1.29

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -4,6 +4,8 @@
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
+/*2的16次方，4路组相连*/
+
 static uint64_t cycle_cnt = 0;
 
 void cycle_increase(int n) { cycle_cnt += n; }
@@ -20,10 +22,14 @@ uint32_t gnum=0;//组数
 
 struct{
   bool valid[MAX_way];//是否有效
+  bool dirty[MAX_way];
   uint16_t tag[MAX_way];//标志位
   uint8_t data[MAX_way][BLOCK_SIZE];//数据
 }cache[MAX_group];
 //定义cache
+
+
+
 
 
 
@@ -36,7 +42,7 @@ struct{
 
 uint32_t cache_read(uintptr_t addr) {
   
-  assert(exp2(addr)<=MEM_SIZE);
+  //assert(exp2(addr)<=MEM_SIZE);
 
   uint32_t g=(addr>>6)%gnum;
 
@@ -58,6 +64,7 @@ uint32_t cache_read(uintptr_t addr) {
   int lucker=rand()%wnum;
   mem_read(addr>>6,cache[g].data[lucker]);
   cache[g].tag[lucker]=(addr>>6)/wnum;
+  if(cache[g].dirty[lucker])mem_write(cache[g].tag[lucker]*wnum,cache[g].data[lucker]);//写回操作
   return cache[g].data[lucker][addr%BLOCK_SIZE];
 
   //return 0;
@@ -66,15 +73,77 @@ uint32_t cache_read(uintptr_t addr) {
 // 例如当 wmask 为 0xff 时，只写入低8比特
 // 若缺失，需要从先内存中读入数据
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  uint32_t g=(addr>>6)%gnum;
+  //找到了
+	for(int i=0;i<wnum;i++){
+    		if(cache[g].valid[i]&&(cache[g].tag[i]==((addr>>6)/wnum)))
+        { 
+          
+          cache[g].dirty[i]=true;
+          uint32_t rnum=(data&wmask);
+          int  j=addr%BLOCK_SIZE;
+          //先当是按照单元来的
+          while(rnum!=0){
+          cache[g].data[i][j]&=(~wmask);
+          cache[g].data[i][j]+=rnum;
+          rnum>>=8;
+          wmask>>=8;
+          j++;
+          }
+
+          break;
+        }
+  }
+  //缺失
+  for(int i=0;i<wnum;i++){
+    if(!cache[g].valid[i]){
+      cache[g].valid[i]=true;
+      mem_read(addr>>6,cache[g].data[i]);
+      cache[g].tag[i]=(addr>>6)/wnum;
+      cache[g].dirty[i]=true;
+
+      uint32_t rnum=(data&wmask);
+      int j=addr%BLOCK_SIZE;
+          //先当是按照单元来的
+          while(rnum!=0){
+          cache[g].data[i][j]&=(~wmask);
+          cache[g].data[i][j]+=rnum;
+          rnum>>=8;
+          wmask>>=8;
+          j++;
+          
+          }
+
+          break;
+    }
+  }
+
+  int lucker=rand()%wnum;
+  mem_read(addr>>6,cache[g].data[lucker]);
+  cache[g].tag[lucker]=(addr>>6)/wnum;
+  uint32_t rnum=(data&wmask);
+  int z=addr%BLOCK_SIZE;
+          //先当是按照单元来的
+          while(rnum!=0){
+          
+          cache[g].data[lucker][z]&=(~wmask);
+          cache[g].data[lucker][z]+=rnum;
+          rnum>>=8;
+          wmask>>=8;
+          z++;
+          }
+
+
+  
 }
 // 初始化一个数据大小为 2^total_size_width B，关联度为 2^associativity_width 的 cache
 // 例如 init_cache(14, 2) 将初始化一个 16KB，4 路组相联的cache
 // 将所有 valid bit 置为无效即可
 void init_cache(int total_size_width, int associativity_width) {
-  assert((1<<associativity_width)<MAX_way);
-  assert((uint64_t)(1<<total_size_width)<(uint64_t)(MAX_group<<6)*(uint64_t)(1<<associativity_width));
+  //assert((exp2(associativity_width))<MAX_way);
+  //assert((uint64_t)(1<<total_size_width)<(uint64_t)(MAX_group<<6)*(uint64_t)(1<<associativity_width));
   
-  wnum=1<<associativity_width;//路数
+  wnum=exp2(associativity_width);//路数
   gnum=((uint64_t)(1<<(total_size_width-6))/wnum);//（组数=总空间/路数/64B）//先不考虑不整除；
 
 
@@ -83,6 +152,7 @@ void init_cache(int total_size_width, int associativity_width) {
   for(int i=0;i<gnum;i++){
     for(int j=0;j<wnum;j++){
       cache[i].valid[j]=false;
+      cache[i].dirty[j]=false;
     }
 
   }
